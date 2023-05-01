d6e0aad3ccbaf526bc8c0eb90b98047ab9c06bfb
Koyomi <fox@9ts.jp>
2023-05-01 21:49:37 +0900

    fix animator-context-menu

--- Editor/AnimatorTools.cs
+++ Editor/AnimatorTools.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEditor;
+using UnityEditor.Animations;
 
-namespace KLabs.Utils
+namespace KLabs.Utils.Editor
 {
 	public static class AnimatorTools
 	{
